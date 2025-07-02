930220cdacc0913489415064dd2913d6c1820d33
vinay <vinaybkrishna10@gmail.com>
2025-07-02 18:44:02 +0530

    new github runner code

--- variable.tf
+++ variable.tf
@@ -2,6 +2,6 @@ variable "env"{}
 variable "vpc_security_group_ids" {}
 # variable "db_instances"{}
 variable "zone_id" {}
-# variable "vault_token" {}
+variable "vault_token" {}
 
 variable "eks" {}
