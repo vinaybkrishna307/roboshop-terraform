032a6da0938f84b75b68375d53810fdf39fc7e53
vinay <vinaybkrishna10@gmail.com>
2025-07-02 18:53:20 +0530

    new github runner code

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-# module "ec2" {
-#     for_each = var.db_instances
-#
-#     source = "./modules/ec2"
-#
-#     ami_id = each.value["ami_id"]
-#     env = var.env
-#     instance_type = each.value["instance_type"]
-#     name = each.key
-#     vpc_security_group_ids = var.vpc_security_group_ids
-#     zone_id = var.zone_id
-#     ansible_role = lookup(each.value,"ansible_role",each.key )
-#     root_volume_size = each.value["root_volume_size"]
-# }
+module "ec2" {
+    for_each = var.db_instances
+
+    source = "./modules/ec2"
+
+    ami_id = each.value["ami_id"]
+    env = var.env
+    instance_type = each.value["instance_type"]
+    name = each.key
+    vpc_security_group_ids = var.vpc_security_group_ids
+    zone_id = var.zone_id
+    ansible_role = lookup(each.value,"ansible_role",each.key )
+    root_volume_size = each.value["root_volume_size"]
+}
 
 module "eks" {
     for_each = var.eks
@@ -21,5 +21,5 @@ module "eks" {
     eks_version = each.value["eks_version"]
     subnets = each.value["subnets"]
     node_groups = each.value["node_groups"]
-     addons = each.value["addons"]
+    addons = each.value["addons"]
 }
--- variable.tf
+++ variable.tf
@@ -1,6 +1,6 @@
 variable "env"{}
 variable "vpc_security_group_ids" {}
-# variable "db_instances"{}
+variable "db_instances"{}
 variable "zone_id" {}
 variable "vault_token" {}
 
