995c8afedfca697b6aa9d63967447f98eaaa61c2
vinay <vinaybkrishna10@gmail.com>
2025-07-03 17:54:40 +0530

    ubuntu

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -47,8 +47,8 @@ resource "null_resource" "frontend" {
 
         connection {
             type     = "ssh"
-            user     = data.vault_generic_secret.ssh.data["username"]
-            password = data.vault_generic_secret.ssh.data["password"]
+            user     = "ec2-user"
+            password = "DevOps321"
             host     = aws_instance.instance.private_ip
         }
 
