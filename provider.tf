6d1dd0ed2bb94c430c1e43def537e94b5df2ecf9
vinay <vinaybkrishna10@gmail.com>
2025-07-03 17:53:02 +0530

    ubuntu

--- provider.tf
+++ provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = "us-east-1" # change if needed
 }
+
+
 provider "vault" {
   address = "http://vault.mikeydevops1.online:8200/"
   token = var.vault_token
--- variable.tf
+++ variable.tf
@@ -2,6 +2,8 @@ variable "env"{}
 variable "vpc_security_group_ids" {}
 variable "db_instances"{}
 variable "zone_id" {}
-variable "vault_token" {}
+variable "vault_token" {
+  type = string
+}
 
 variable "eks" {}
